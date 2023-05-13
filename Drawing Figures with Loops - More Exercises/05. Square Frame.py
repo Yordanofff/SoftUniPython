b3d7a5e82523eb2fd5d7e2e95cce54423949c952
Yordanofff <ivailo.yordanov.ph@gmail.com>
2023-05-14 02:52:56 +0300

    fixed

--- Drawing Figures with Loops - More Exercises/05. Square Frame.py
+++ Drawing Figures with Loops - More Exercises/05. Square Frame.py
@@ -1,11 +1,11 @@
 n = int(input())
-if n == 2:
-    print(" ".join("+" * 2))  # todo - doesn't work no matter what I try...
-else:
-    for i in range(1, n + 1):
-        num_dashes = n - 2
-        if i == 1 or i == n:
-            symbol = "+"
-        else:
-            symbol = "|"
+for i in range(1, n + 1):
+    num_dashes = n - 2
+    if i == 1 or i == n:
+        symbol = "+"
+    else:
+        symbol = "|"
+    if n == 2:
+        print(f"{symbol} {symbol}")
+    else:
         print(f"{symbol} {' '.join('-' * num_dashes)} {symbol}")
