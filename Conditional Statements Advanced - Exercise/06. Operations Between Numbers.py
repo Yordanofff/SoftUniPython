26cc3169540e3f6fc4afaae2f97f5072c962828d
Yordanofff <ivailo.yordanov.ph@gmail.com>
2023-05-17 12:33:01 +0300

    moved to a func to remove the exit..

--- Conditional Statements Advanced - Exercise/06. Operations Between Numbers.py
+++ Conditional Statements Advanced - Exercise/06. Operations Between Numbers.py
@@ -1,20 +1,22 @@
-num1 = int(input())
-num2 = int(input())
-operator = input()
+def print_operations_between_nums(num1, num2, operator):
+    if num2 == 0:
+        print(f"Cannot divide {num1} by zero")
+        return
 
-if num2 == 0:
-    print(f"Cannot divide {num1} by zero")
-    exit(1)
-result = eval(str(num1) + operator + str(num2))
+    result = eval(str(num1) + operator + str(num2))
 
-odd_or_even = "odd"
-if result % 2 == 0:
-    odd_or_even = "even"
+    odd_or_even = "even" if result % 2 == 0 else "odd"
 
-if operator == "/":
-    print(f"{num1} {operator} {num2} = {result:.2f}")
-elif operator == "%":
-    print(f"{num1} {operator} {num2} = {result}")
-else:
-    print(f"{num1} {operator} {num2} = {result} - {odd_or_even}")
+    if operator == "/":
+        print(f"{num1} {operator} {num2} = {result:.2f}")
+    elif operator == "%":
+        print(f"{num1} {operator} {num2} = {result}")
+    else:
+        print(f"{num1} {operator} {num2} = {result} - {odd_or_even}")
 
+
+n1 = int(input())
+n2 = int(input())
+op = input()
+
+print_operations_between_nums(n1, n2, op)
